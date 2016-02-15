d20916eedb17834c0e8b35ca9ea3faeb89944069
Josh <t3rdtp@hotmail.com>
2016-02-14 19:23:58 -0700

    String Calculator Kata
    TDD approach to solving the string calculator kara

---
+++ StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+            else if (!numbers.Contains(","))
+                return int.Parse(numbers);
+            else
+                return AddNumbers(numbers);
+        }
+
+        private int AddNumbers(string numbers)
+        {
+            string[] splitNumbers = GetRegexArray(numbers);
+            return AddArrayContents(splitNumbers);
+        }
+
+        private int AddArrayContents(string[] splitNumbers)
+        {
+            bool result;
+            int sum = 0;
+            int number;
+            for (int i = 0; i < splitNumbers.Length; i++)
+            {
+                result = int.TryParse(splitNumbers[i], out number);
+                if (number < 0)
+                    throw new NoNegativesAllowedException();
+                if (result)
+                    sum += int.Parse(splitNumbers[i]);
+            }
+            return sum;
+        }
+
+        private string[] GetRegexArray(string numbers)
+        {
+            string pattern = @"(,)|(\n)";
+            Regex regex = new Regex(pattern);
+            string[] splitNumbers = regex.Split(numbers);
+
+            return splitNumbers;
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalulatorTests/StringCalculatorTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorKata;
+
+namespace StringCalulatorTests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void AddsEmptyString()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void AddsOneNumber()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbers()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddsThreeNumbers()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1,2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void AddsFourNumbers()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1,2,3,4");
+
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void AddsNumbersWithNewLines()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1\n2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void AddsNumbersWithNewLinesAndCommasMixed()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1\n2,3,\n,4,\n5");
+
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NoNegativesAllowedException))]
+        public void NegativeNumberThrowsAnException()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1,2,3,-4");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NoNegativesAllowedException))]
+        public void MultipleNegativeNumbersThrowsAnException()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1,2,3,-4,5,-10");
+        }
+    }
+}
